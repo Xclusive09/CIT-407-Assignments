e2eeb6247a3f326f7b82ab0181b52e2d775d9e59
Abdulquddus Ibrahim <Ibrahimabdulquddus51@gmail.com>
2025-05-07 16:58:19 +0100

    array of names to loop through and print greetings

---
+++ src/task6.kt
@@ -0,0 +1,9 @@
+fun main() {
+    // Create an array of 5 names
+    val names = arrayOf("Alice", "Bob", "Charlie", "Xclusive", "Feedy")
+
+    // Loop through the array and print greetings
+    for (name in names) {
+        println("Hello, $name")
+    }
+}
