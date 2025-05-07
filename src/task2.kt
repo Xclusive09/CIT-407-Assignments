ee5e7643c50f07b1141f730df4aea292aa40614c
Abdulquddus Ibrahim <Ibrahimabdulquddus51@gmail.com>
2025-05-07 16:16:49 +0100

    Add username input prompt and greeting message

---
+++ src/task2.kt
@@ -0,0 +1,12 @@
+fun main() {
+    // Prompt user for name
+    print("Enter your name: ")
+    val name = readLine()
+
+    // Prompt user for age
+    print("Enter your age: ")
+    val age = readLine()
+
+    // Print greeting with name and age
+    println("Hello $name, you are $age years old!")
+}
